0f10eb298758b13fdb5fbf695d6a415460e31dad
HimeWoo <aogata15@gmail.com>
2024-08-07 14:35:04 -0700

    Entities out of view are culled

--- sources/editor.c
+++ sources/editor.c
@@ -1,4 +1,5 @@
 #include "editor.h"
+#include "entity.h"
 
 // ----------------------------------------------------------------------------
 // Constants
@@ -17,6 +18,8 @@ Rectangle toolPanel;
 Rectangle editorPanel;
 Camera2D editorCam;
 Rectangle testButton;
+Color testButtonColor;
+Texture2D testTexture;
 
 void InitEditor(void) {
     entities = malloc(0);
@@ -44,6 +47,8 @@ void InitEditor(void) {
         toolPanel.x + toolBorderThickness, toolPanel.y + toolBorderThickness,
         toolPanel.width - 2 * toolBorderThickness, toolPanel.width
     };
+    testButtonColor = WHITE;
+    testTexture = LoadTexture(ASSETS_PATH "testimage.png");
 }
 
 Vector2 SnapToGrid(Vector2 pos, Vector2 offset) {
@@ -87,30 +92,43 @@ void UpdateEditor() {
         editorPanel.width = GetScreenWidth() - toolPanel.width;
         editorPanel.height = GetScreenHeight();
     }
+    Vector2 mousePos = GetMousePosition();
+    Vector2 mouseWorldPos = GetScreenToWorld2D(mousePos, editorCam);
     Vector2 mouseDelta = GetMouseDelta();
-    if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) {
-        Vector2 delta = Vector2Scale(mouseDelta, -1.0f / editorCam.zoom);
-        editorCam.target = Vector2Add(editorCam.target, delta);
-    }
-    float wheel = GetMouseWheelMove();
-    if (wheel != 0) {
-        const float ZOOM_INCREMENT = 0.05f;
-        const float MIN_ZOOM = 0.1f;
-        const float MAX_ZOOM = 2.0f;
+    if (CheckCollisionPointRec(mousePos, editorPanel)) {
+        if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) {
+            Vector2 delta = Vector2Scale(mouseDelta, -1.0f / editorCam.zoom);
+            editorCam.target = Vector2Add(editorCam.target, delta);
+        }
+        if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
+            printf("Mouse up: -------------------------------\n");
+            printf("\t- Before:\tnumEntities = %d\n", numEntities);
+            Vector2 snappedPos =  SnapToGrid(mouseWorldPos, Vector2Zero());
+            Entity *e = NewEntity(snappedPos, testTexture, 0.0f, 0.1f);
+            numEntities++;
+            entities = realloc(entities, numEntities * sizeof(Entity));
+            entities[numEntities - 1] = e;
+            printf("\t- After:\tnumEntities = %d\n", numEntities);
+        } 
+        float wheel = GetMouseWheelMove();
+        if (wheel != 0) {
+            const float ZOOM_INCREMENT = 0.05f;
+            const float MIN_ZOOM = 0.1f;
+            const float MAX_ZOOM = 2.0f;
 
-        Vector2 mousePos = GetMousePosition();
-        Vector2 mouseWorldPos = GetScreenToWorld2D(mousePos, editorCam);
-        editorCam.offset = mousePos;
-        editorCam.target = mouseWorldPos;
-        float targetZoom = editorCam.zoom + wheel * ZOOM_INCREMENT;
-        editorCam.zoom = Clamp(targetZoom, MIN_ZOOM, MAX_ZOOM);
+            editorCam.offset = mousePos;
+            editorCam.target = mouseWorldPos;
+            float targetZoom = editorCam.zoom + wheel * ZOOM_INCREMENT;
+            editorCam.zoom = Clamp(targetZoom, MIN_ZOOM, MAX_ZOOM);
+        }
     }
 }
 
 void DrawEditor() {
     BeginMode2D(editorCam);
 
-        BeginScissorMode(editorPanel.x, editorPanel.y, editorPanel.width, editorPanel.height);
+        BeginScissorMode(   editorPanel.x, editorPanel.y, 
+                            editorPanel.width, editorPanel.height);
 
             const Color EDITOR_BG_COLOR = BLACK;
             ClearBackground(EDITOR_BG_COLOR);
@@ -138,16 +156,58 @@ void DrawEditor() {
             // Draw grid lines in world space
             DrawGridLines(panelWorld);
             // Draw components
-            Vector2 mousePos = GetMousePosition();
-            Vector2 mouseWorldPos = GetScreenToWorld2D(mousePos, editorCam);
-            DrawCircleV(
-                SnapToGrid(mouseWorldPos, Vector2Zero()),
-                10, WHITE
-            );
+            for (int i = 0; i < numEntities; i++) {
+                Entity e = *(entities[i]);
+                Rectangle entityRec = CLITERAL(Rectangle) {
+                    e.pos.x, e.pos.y,
+                    e.scale * e.tex.width, e.scale * e.tex.height
+                };
+                Vector2 topLeftWorld = CLITERAL(Vector2) {
+                    entityRec.x, entityRec.y
+                };
+                Vector2 bottomRightWorld = CLITERAL(Vector2) {
+                    entityRec.x + entityRec.width,
+                    entityRec.y + entityRec.height
+                };
+                Vector2 topLeft = GetWorldToScreen2D(
+                    topLeftWorld, editorCam
+                );
+                Vector2 bottomRight = GetWorldToScreen2D(
+                    bottomRightWorld, editorCam
+                );
+                Rectangle entityWorldRec = CLITERAL(Rectangle) {
+                    topLeft.x, topLeft.y,
+                    bottomRight.x - topLeft.x, bottomRight.y - topLeft.y
+                };
+                if (CheckCollisionRecs(entityWorldRec, editorPanel)) {
+                    printf("Entity <%d>: ---------\n", i);
+                    printf(
+                        "\t- top left:\t(%.2f, %.2f)\n",
+                        entityWorldRec.x, 
+                        entityWorldRec.y
+                    );
+                    DrawEntity(entities[i], WHITE);
+                    DrawRectangleLinesEx(entityRec, 4, RED);
+                }
+            }
 
         EndScissorMode();
 
     EndMode2D();
     DrawRectangleLinesEx(editorPanel, toolBorderThickness, toolBorderColor);
+    // test button --------------------
+    if (CheckCollisionPointRec(GetMousePosition(), testButton)) {
+        if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
+            testButtonColor = DARKGRAY;
+        } else {
+            testButtonColor = GRAY;
+        }
+        if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
+            printf("Button clicked\n");
+        }
+    }
+    DrawRectangleRec(testButton, testButtonColor);
+    DrawRectangleLinesEx(testButton, 3.0, BLACK);
+    // --------------------------------
     DrawRectangleLinesEx(toolPanel, toolBorderThickness, toolBorderColor);
 }
--- sources/gateSimulator.c
+++ sources/gateSimulator.c
@@ -1,6 +1,5 @@
 #include <raylib.h>
 #include <rlgl.h>
-#include "entity.h"
 #include "editor.h"
 
 // ----------------------------------------------------------------------------
@@ -31,46 +30,10 @@ int main(void) {
     
     Init();
 
-    Texture2D texture = LoadTexture(ASSETS_PATH "testimage.png");
-
     // Main Loop --------------------------------------------------------------
     while(!WindowShouldClose()) {
-        Vector2 mousePos = GetMousePosition();
-        Vector2 mouseDelta = GetMouseDelta();
-
         Update();
         Draw();
-
-        /* BeginDrawing();
-
-            const Color guiBGColor = {0x5c, 0x57, 0x57, 0xff};
-            ClearBackground(guiBGColor);
-
-            DrawEditor(editorPanel, &editorCam);
-            
-            if (CheckCollisionPointRec(mousePos, editorPanel)) {
-                Vector2 mouseWorldPos = GetScreenToWorld2D(mousePos, editorCam);
-                Vector2 gridPos = SnapToGrid(mouseWorldPos, Vector2Zero());
-                printf("Grid pos: (%.2f, %.2f)\n", gridPos.x, gridPos.y);
-            }
-
-            Color testButtonColor = WHITE;
-            if (CheckCollisionPointRec(mousePos, testButton)) {
-                if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
-                    testButtonColor = DARKGRAY;
-                } else {
-                    testButtonColor = GRAY;
-                }
-                if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
-                    printf("Button clicked\n");
-                }
-            }
-            DrawRectangleRec(testButton, testButtonColor);
-            DrawRectangleLinesEx(testButton, 4.0, BLACK);
-            
-            DrawFPS(0, 0);
-
-        EndDrawing(); */
     }
     // ------------------------------------------------------------------------
 
